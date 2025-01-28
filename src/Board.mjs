eda67eb94948600bb276135d22a37be535cd6008
anastat <anastasiia.tikhonova@mail.com>
2025-01-28 20:46:06 +0200

    Level 0.

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,17 +1,22 @@
 export class Board {
   width;
   height;
+  boardArr;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.boardArr = Array(height)
+      .fill(".")
+      .map(() => Array(width).fill("."));
   }
 
   toString() {
-    let str = '';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        str += ".";
+    let str = "";
+
+    for (let i = 0; i < this.boardArr.length; i++) {
+      for (let j = 0; j < this.boardArr[i].length; j++) {
+        str += this.boardArr[i][j];
       }
       str += "\n";
     }
