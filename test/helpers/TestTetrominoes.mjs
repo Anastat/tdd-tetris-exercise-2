519574f434e003c2577f0ff22a6bbcfd37fb6b9c
anastat <anastasiia.tikhonova@mail.com>
2025-05-04 22:42:15 +0300

    Added more test shapes.

--- test/helpers/TestTetrominoes.mjs
+++ test/helpers/TestTetrominoes.mjs
@@ -53,6 +53,35 @@ const O_SHAPE = [
   ["O", "O"],
 ];
 
+const L_SHAPE = [
+  ["L", "L", "L"],
+  ["L", ".", "."],
+];
+
+const L_SHAPE_ORIENTATIONS = [
+  [
+    [".", ".", ".", "."],
+    ["L", "L", "L", "."],
+    ["L", ".", ".", "."],
+  ],
+  [
+    ["L", "L", ".", "."],
+    [".", "L", ".", "."],
+    [".", "L", ".", "."],
+  ],
+  [
+    [".", ".", ".", "."],
+    [".", ".", "L", "."],
+    ["L", "L", "L", "."],
+  ],
+  [
+    [".", "L", ".", "."],
+    [".", "L", ".", "."],
+    [".", "L", "L", "."],
+  ],
+];
+
 export const TestTShape = new Tetromino(T_SHAPE_ROTATIONS, T_SHAPE);
 export const TestIShape = new Tetromino(I_SHAPE_ROTATIONS, I_SHAPE);
 export const TestOShape = new Tetromino([O_SHAPE], O_SHAPE);
+export const TestLShape = new Tetromino(L_SHAPE_ORIENTATIONS, L_SHAPE);
