64569d45c9367fc3627695e787e6340fe42c2e87
anastat <anastasiia.tikhonova@mail.com>
2025-05-05 20:59:58 +0300

    Added onEvent handler to ScoreCount.

--- src/ScoreCount.mjs
+++ src/ScoreCount.mjs
@@ -1,5 +1,16 @@
+import { EventTypes } from "./constants/EventTypes.mjs";
 export class ScoreCount {
   constructor() {
     this.score = 0;
+  }
+
+  onEvent(event) {
+    if (event.type === EventTypes.ROWS_CLEARED) {
+      this.score += ScoreCount.getScoreForRows(event.count);
+    }
+  }
+
+  static getScoreForRows(rows) {
+    return 0;
   }
 }
