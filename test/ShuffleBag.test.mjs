976af836e57695fc4bd3499c81f43b9d6fa9146c
anastat <anastasiia.tikhonova@mail.com>
2025-05-06 10:44:27 +0300

    Added tests to validate that it bag repeats once empty and order is different in 2 rounds.

--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -10,10 +10,44 @@ describe("Shuffle bag", () => {
     tetrominoes.forEach((tetromino) => bag.add(tetromino));
     const pulled = [];
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < tetrominoes.length; i++) {
       pulled.push(bag.next());
     }
 
     expect(new Set(pulled)).to.eql(new Set(tetrominoes));
+  });
+
+  test("tetrominoes repeat in second round", () => {
+    const bag = new ShuffleBag();
+    tetrominoes.forEach((tetromino) => bag.add(tetromino));
+    const firstRound = [];
+    const secondRound = [];
+
+    for (let i = 0; i < tetrominoes.length; i++) {
+      firstRound.push(bag.next());
+    }
+
+    for (let i = 0; i < tetrominoes.length; i++) {
+      secondRound.push(bag.next());
+    }
+
+    expect(new Set(firstRound)).to.eql(new Set(secondRound));
+  });
+
+  test("tetrominoes in different order in 2 rounds", () => {
+    const bag = new ShuffleBag();
+    tetrominoes.forEach((tetromino) => bag.add(tetromino, 5));
+    const firstRound = [];
+    const secondRound = [];
+
+    for (let i = 0; i < bag.blocks.length; i++) {
+      firstRound.push(bag.next());
+    }
+
+    for (let i = 0; i < bag.blocks.length; i++) {
+      secondRound.push(bag.next());
+    }
+
+    expect(firstRound).to.not.eql(secondRound); // There is a chance that it might fail
   });
 });
